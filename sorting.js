6d9e574d2d7843ea38956cf5a257fac703670b9d
Liam Kaufman Simpkins <liamks@ldk.local>
2010-05-30 16:51:43 -0400

    begin modification to object oriented version

--- sorting.js
+++ sorting.js
@@ -11,6 +11,80 @@ var curr_i = null;
 var curr_j = null;
 
 
+function Sorter(){
+  this.rect_array = [];
+  this.i = 0; this.j= 1;
+  
+  this.prev_i = null;
+  this.prev_j = null;
+  this.curr_i = null;
+  this.curr_j = null;
+}
+Sorter.prototype.swap = function(){
+  var temp_curr_j_x = this.curr_j.data('x');
+  var temp_curr_i_x = this.curr_i.data('x');
+  this.curr_j.data('x', temp_curr_i_x).animate({'left': temp_curr_i_x},20);
+  this.curr_i.data('x', temp_curr_j_x).animate({'left': temp_curr_j_x},20);
+  
+  var temp_current = this.rect_array[this.j];
+  this.rect_array[this.j] = this.rect_array[this.i];
+  this.rect_array[this.i] = temp_current;
+  
+}
+
+Sorter.prototype.mark_i_j = function(){
+  console.log('mark');
+  if (this.prev_i != null){
+    this.prev_i.removeClass('highlight');
+    this.prev_j.removeClass('highlight');
+    this.prev_i = this.curr_i;
+    this.prev_j = this.curr_j;
+  }else{
+    this.prev_i = $(this.rect_array[this.i]);
+    this.prev_j = $(this.rect_array[this.j]);
+  }
+  this.curr_i = $(this.rect_array[this.i]);
+  this.curr_j = $(this.rect_array[this.j]);
+  $(this.curr_i).addClass('highlight');
+  $(this.curr_j).addClass('highlight');
+  
+}
+
+
+Sorter.prototype.init_bubblesort = function(){
+  this.end = this.rect_array.length;
+  this.bubblesort();
+}
+
+Sorter.prototype.bubblesort = function(){
+  this.mark_i_j();
+  if(this.curr_i.data('height') > this.curr_j.data('height')){
+    this.swap();
+  }
+  this.i += 1;
+  this.j += 1;
+  if(this.j == this.end){
+    this.i = 0;
+    this.j = 1;
+    this.end -= 1;
+    if(this.end == 1){return}
+  }
+  var self = this;
+  //window.setTimeout(this.bubblesort, 80);
+  window.setTimeout(function(){self.bubblesort();}, 40);
+}
+
+Sorter.prototype.init = function(array,sort_type){
+  var sort_types = {'bubblesort':this.init_bubblesort,
+                    'quicksort':this.init_quicksort,
+                    'liamsort':this.init_liamsort};
+  this.rect_array = array;
+  sort_types[sort_type].call(this);
+}
+
+
+
+
 function iterate(){
   $(rect_array[i]).addClass('current');
   var temp = null;
@@ -50,8 +124,10 @@ function bubblesort(array){
 }
 
 function sortit() {
-  console.log('called');
-  bubblesort($('#container div'));
+  var sorter = new Sorter();
+  sorter.init($('#container div'), $('#sorttype').val())
+  //console.log('called');
+  //bubblesort($('#container div'));
 }
 
 function reset(){
